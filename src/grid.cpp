beb7e928a7bff103007701b1a97cc1fa0d78110d
Noitorra <dimaxxru@gmail.com>
2014-04-05 00:34:29 +0400

    Repair good merge

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,12 +1,3 @@
 #include "grid.h"
 
-<<<<<<< HEAD
-bool Grid::WriteFile(const std::string& name) {
-  return true;
-}
 
-bool Grid::ReadFile(const std::string& name) {
-  return true;
-}
-=======
->>>>>>> e1934b81214e36cbc6a9fd3a20a371990139654e
