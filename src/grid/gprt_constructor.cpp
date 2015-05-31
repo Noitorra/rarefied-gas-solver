10ed70eff6b663d931437b995d6f048f8351d5d1
prohor33 <prohor33@gmail.com>
2015-05-31 14:16:14 +0300

    set min l to not let oscillations appear

--- src/grid/gprt_constructor.cpp
+++ src/grid/gprt_constructor.cpp
@@ -23,7 +23,9 @@ void GridConstructor::ConfigureGPRT() {
     Config::m_normalize = 133 * 1.66e-27;   // kg
     Config::e_cut_normalize = sqrt(sep::k * Config::T_normalize / Config::m_normalize); // m / s
     //Config::l_normalize = 2.78e-5; // m
-	Config::l_normalize = 0.44; // m
+	//Config::l_normalize = 6e-5; // m -	Diman count it
+	Config::l_normalize = 0.5 * 6e-4; // m +
+	//Config::l_normalize = 0.2 * 6e-4; // m -
     Config::tau_normalize = Config::l_normalize / Config::e_cut_normalize;  // s
 
     T1 /= Config::T_normalize;
